5e9bd707b6781bdd78015b5ba177959825d258a7
Xander Deng <dengxiang2010@gmail.com>
2017-04-25 11:18:31 +0800

    single target

--- Sources/Supporting Files/GenericID.h
+++ Sources/Supporting Files/GenericID.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 ddddxxx. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for GenericID.
 FOUNDATION_EXPORT double GenericIDVersionNumber;
