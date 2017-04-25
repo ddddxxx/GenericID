39c121f9a57bd39ca774d2a7e7ae66733360631c
Xander Deng <dengxiang2010@gmail.com>
2017-04-25 12:25:09 +0800

    update: test case

---
+++ Tests/GenericIDTests/AssociatedObjectTests.swift
@@ -0,0 +1,39 @@
+//
+//  AssociatedObjectTests.swift
+//  GenericIDTests
+//
+//  Created by 邓翔 on 2017/4/25.
+//  Copyright © 2017年 ddddxxx. All rights reserved.
+//
+
+import XCTest
+@testable import GenericID
+
+class AssociatedObjectTests: XCTestCase {
+    
+    func testValueType() {
+        let obj = NSObject()
+        XCTAssertNil(obj.associatedValue(for: .ValueTypeKey))
+        obj.set(associatedValue: 10, for: .ValueTypeKey)
+        XCTAssert(obj.associatedValue(for: .ValueTypeKey) == 10)
+        obj.set(associatedValue: 20, for: .ValueTypeKey)
+        XCTAssert(obj.associatedValue(for: .ValueTypeKey) == 20)
+    }
+    
+    func testReferenceType() {
+        let obj = NSObject()
+        var date = Date()
+        XCTAssertNil(obj.associatedValue(for: .ReferenceTypeKey))
+        obj.set(associatedValue: date, for: .ReferenceTypeKey)
+        XCTAssert(obj.associatedValue(for: .ReferenceTypeKey) == date)
+        date = Date()
+        obj.set(associatedValue: date, for: .ReferenceTypeKey)
+        XCTAssert(obj.associatedValue(for: .ReferenceTypeKey) == date)
+    }
+    
+}
+
+extension NSObject.AssociatedKeys {
+    static let ValueTypeKey: Key<Int>       = "ValueTypeKey"
+    static let ReferenceTypeKey: Key<Date>  = "ReferenceTypeKey"
+}
