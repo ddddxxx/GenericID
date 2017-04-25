951a596137efcc5d90824c98ed4c50afbe2b65e9
Xander Deng <dengxiang2010@gmail.com>
2017-04-25 10:58:57 +0800

    Initial Commit

---
+++ GenericID/GenericID.h
@@ -0,0 +1,19 @@
+//
+//  GenericID.h
+//  GenericID
+//
+//  Created by 邓翔 on 2017/4/25.
+//  Copyright © 2017年 ddddxxx. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GenericID.
+FOUNDATION_EXPORT double GenericIDVersionNumber;
+
+//! Project version string for GenericID.
+FOUNDATION_EXPORT const unsigned char GenericIDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericID/PublicHeader.h>
+
+
