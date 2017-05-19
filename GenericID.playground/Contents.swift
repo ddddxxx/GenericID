c247544259f82e5b769e7e55de4f6bb10b5ea5fa
Xander Deng <dengxiang2010@gmail.com>
2017-05-19 10:13:46 +0800

    update: playground

--- GenericID.playground/Contents.swift
+++ GenericID.playground/Contents.swift
@@ -1,2 +1,18 @@
 import Cocoa
 import GenericID
+
+let ud = UserDefaults.standard
+
+extension UserDefaults.DefaultKeys {
+    static let colorKey: Key<NSColor?> = "colorKey"
+    static let pointKey: Key<CGPoint?> = "pointKey"
+}
+
+ud.archive(#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), for: .colorKey)
+ud.unarchive(.colorKey)
+
+let p = CGPoint(x: 1, y: 1)
+
+ud.wrap(p, for: .pointKey)
+ud.unwrap(.pointKey)
+
