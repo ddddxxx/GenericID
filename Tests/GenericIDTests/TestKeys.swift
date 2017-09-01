d8700ce95f94f883b2a00616a0e908238c25caed
Xander Deng <dengxiang2010@gmail.com>
2017-09-01 13:46:44 +0800

    test: update test case

--- Tests/GenericIDTests/TestKeys.swift
+++ Tests/GenericIDTests/TestKeys.swift
@@ -41,15 +41,15 @@ extension UserDefaults.DefaultKeys {
     static let FloatOptKey: Key<Float?>             = "FloatOptKey"
     static let DoubleOptKey: Key<Double?>           = "DoubleOptKey"
     static let StringOptKey: Key<String?>           = "StringOptKey"
-    static let URLOptKey: Key<URL?>                 = "URLOptKey"
+    static let URLOptKey: ArchivedKey<URL?>         = "URLOptKey"
     static let DateOptKey: Key<Date?>               = "DateOptKey"
     static let DataOptKey: Key<Data?>               = "DataOptKey"
     static let ArrayOptKey: Key<[Any]?>             = "ArrayOptKey"
     static let StringArrayOptKey: Key<[String]?>    = "StringArrayOptKey"
     static let DictionaryOptKey: Key<[String: Any]?> = "DictionaryOptKey"
-    static let ColorOptKey: Key<Color?>             = "ColorOptKey"
-    static let RectOptKey: Key<CGRect?>             = "RectOptKey"
-    static let AnyOptKey: Key<Any?>                 = "AnyOptKey"
+    static let ColorOptKey: ArchivedKey<Color>      = "ColorOptKey"
+    static let RectOptKey: JSONCodedKey<CGRect>     = "RectOptKey"
+    static let AnyOptKey: Key<Any>                  = "AnyOptKey"
 }
 
 extension NSObject.AssociateKeys {
