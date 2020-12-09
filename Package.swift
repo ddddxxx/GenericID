31594c0f73306bb338316d7beec505e9b8b40dc1
Xander Deng <dengxiang2010@gmail.com>
2020-12-09 10:03:10 +0800

    use cx 0.3.0

--- Package.swift
+++ Package.swift
@@ -1,25 +1,21 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "GenericID",
-    platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2),
-    ],
     products: [
         .library(
             name: "GenericID",
             targets: ["GenericID"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/cx-org/CombineX", .upToNextMinor(from: "0.2.0"))
+        .package(url: "https://github.com/cx-org/CombineX", .upToNextMinor(from: "0.3.0"))
     ],
     targets: [
         .target(
             name: "GenericID",
-            dependencies: ["CXShim"]),
+            dependencies: [.product(name: "CXShim", package: "CombineX")]),
         .testTarget(
             name: "GenericIDTests",
             dependencies: ["GenericID"]),
