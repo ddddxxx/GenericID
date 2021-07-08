c6da97cd19899174cde6a14c7a46c794cd5d9cf8
Xander Deng <dengxiang2010@gmail.com>
2021-07-08 20:26:20 +0800

    update dependency

--- Package.swift
+++ Package.swift
@@ -10,14 +10,50 @@ let package = Package(
             targets: ["GenericID"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/cx-org/CombineX", .upToNextMinor(from: "0.3.0"))
+        .package(url: "https://github.com/cx-org/CXShim", .upToNextMinor(from: "0.4.0"))
     ],
     targets: [
-        .target(
-            name: "GenericID",
-            dependencies: [.product(name: "CXShim", package: "CombineX")]),
-        .testTarget(
-            name: "GenericIDTests",
-            dependencies: ["GenericID"]),
+        .target(name: "GenericID", dependencies: ["CXShim"]),
+        .testTarget(name: "GenericIDTests", dependencies: ["GenericID"]),
     ]
 )
+
+enum CombineImplementation {
+    
+    case combine
+    case combineX
+    case openCombine
+    
+    static var `default`: CombineImplementation {
+        #if canImport(Combine)
+        return .combine
+        #else
+        return .combineX
+        #endif
+    }
+    
+    init?(_ description: String) {
+        let desc = description.lowercased().filter { $0.isLetter }
+        switch desc {
+        case "combine":     self = .combine
+        case "combinex":    self = .combineX
+        case "opencombine": self = .openCombine
+        default:            return nil
+        }
+    }
+}
+
+extension ProcessInfo {
+
+    var combineImplementation: CombineImplementation {
+        return environment["CX_COMBINE_IMPLEMENTATION"].flatMap(CombineImplementation.init) ?? .default
+    }
+}
+
+import Foundation
+
+let combineImpl = ProcessInfo.processInfo.combineImplementation
+
+if combineImpl == .combine {
+    package.platforms = [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)]
+}
